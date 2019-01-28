16365d64c2a12b5838f12a71a83a82ef3adabd67
SAP0312 <shubhamapatidar143@gmail.com>
2019-01-28 19:00:32 +0530

    Adding Counter

--- src/main/java/com/reducer/WordCountReducer.java
+++ src/main/java/com/reducer/WordCountReducer.java
@@ -1,5 +1,6 @@
 package com.reducer;
 
+import com.utils.Counters;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -11,14 +12,20 @@ import java.io.IOException;
 // Reducer for Word Count
 public class WordCountReducer extends Reducer<Text, IntWritable,Text,IntWritable> { //  Reducer for wordCount
 
-    public void reduce(Text text, Iterable<IntWritable> values, Context context) throws IOException,InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,InterruptedException {
 
         int count=0;
 
         for(IntWritable value:values){
-        count+=value.get();
+        count++;
         }
-        context.write(text,new IntWritable(count));
+        context.write(key,new IntWritable(count));
+        Character firstChar = key.toString().charAt(0);
 
+        if(Character.isDigit(firstChar)){
+            context.getCounter("UserDefined Counter", Counters.Words.DIGIT.name()).increment(1);
+        }
+        else
+            context.getCounter("User Defined Counters",Counters.Words.ALPHABET.name()).increment(1);
     }
 }
